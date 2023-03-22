a4d49886d28b680ad56eca3e34c59fd1900c990a
pedroojanuu <93834123+pedroojanuu@users.noreply.github.com>
2023-03-22 15:55:09 +0000

    Teacher's UCs and UC's name webscraping implemented.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TeachMeWell',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'TeachMeWell'),
     );
   }
 }
