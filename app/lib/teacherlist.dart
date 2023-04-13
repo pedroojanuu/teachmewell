ffd64159b7a91ca518ee46db5610441a9b3bd44f
pedroojanuu <93834123+pedroojanuu@users.noreply.github.com>
2023-04-13 19:05:21 +0100

    Retrieving teachers names. Missing printing faculty on list.

--- app/lib/teacherlist.dart
+++ app/lib/teacherlist.dart
@@ -1,16 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
+class Pair<T1, T2> {
+  final T1 a;
+  final T2 b;
+
+  Pair(this.a, this.b);
+}
+
 class AllTeachersPage extends StatefulWidget {
   @override
-  _AllTeachersPageState createState() => _AllTeachersPageState();
+  _AllTeachersPageState createState() {
+    return new _AllTeachersPageState();
+  }
+}
+
+Future<List<Pair<String, String>>> getTeachersNames() async {
+  List<Pair<String, String>> l = [];
+  var querySnapshot = await FirebaseFirestore.instance.collection("professor").get();
+
+  for (var element in querySnapshot.docs) {
+    String name = element.data()['nome'].toString();
+    String faculty = element.data()['faculdade'].toString();
+    l.add(Pair<String, String>(name, faculty));
+  }
+
+  return l;
 }
 
 class _AllTeachersPageState extends State<AllTeachersPage> {
   TextEditingController _textEditingController = TextEditingController();
 
-  List<String> testListOnSearch = [];
-  List<String> testList = ["Euskal Herria", "Portugal", "Deutschland", "Catalunya", "United States", "Australia", "Galiza", "Andorra", "Cuba", "Brasil", "Colombia", "Uruguay", "Chile", "Italia"];
+  List<Pair<String, String>> nameListOnSearch = [];
+  List<Pair<String, String>> nameList = [];
+
+  _AllTeachersPageState() {
+    getTeachersNames().then((val) => setState(() {
+      nameList = val;
+    }));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,9 +53,7 @@ class _AllTeachersPageState extends State<AllTeachersPage> {
             child: TextField(
               onChanged: (value) {
                 setState(() {
-                  testListOnSearch = testList
-                      .where((element) => element.toLowerCase().contains(value.toLowerCase()))
-                      .toList();
+                  nameListOnSearch = search(nameList, _textEditingController.text, 2);
                 });
               },
               controller: _textEditingController,
@@ -39,7 +66,7 @@ class _AllTeachersPageState extends State<AllTeachersPage> {
           )
         ),
       ),
-      body: testListOnSearch.isEmpty && _textEditingController!.text.isNotEmpty?
+      body: nameListOnSearch.isEmpty && _textEditingController!.text.isNotEmpty?
           Center (
             child:
               Column(
@@ -55,68 +82,107 @@ class _AllTeachersPageState extends State<AllTeachersPage> {
                   ]
               ),
           ) :
-      ListView.builder(
-          itemCount: _textEditingController!.text.isNotEmpty? testListOnSearch.length : testList.length,
-          itemBuilder: (_, index) {
-            return Row(
-              children: [
-                SizedBox(height: 50,),
-                Text(
-                  _textEditingController!.text.isNotEmpty?
-                  testListOnSearch[index] : testList[index],
-                  style: TextStyle(fontSize: 20),),
-              ],
-            );
-          },
-        )
+        ListView.builder(
+            itemCount: _textEditingController!.text.isNotEmpty? nameListOnSearch.length : nameList.length,
+            itemBuilder: (_, index) {
+              return Row(
+                children: [
+                  SizedBox(height: 50,),
+                  Row(
+                    children: [
+                      SizedBox(height: 40,),
+                      SizedBox(
+                        width: (MediaQuery.of(context).size.width)-5,
+                        child: Text(
+                          overflow: TextOverflow.ellipsis,
+                          _textEditingController!.text.isNotEmpty? nameListOnSearch[index].a : nameList[index].a,
+                          style: TextStyle(fontSize: 20,),
+                        ),
+                      ),
+                    ],
+                  ),
+                  /*Row(
+                    children: [
+                      SizedBox(height: 10,),
+                      Text(
+                        _textEditingController!.text.isNotEmpty? nameListOnSearch[index].b : nameList[index].b,
+                        style: TextStyle(fontSize: 13,),
+                      )
+                    ],
+                  ),*/
+                ],
+              );
+            },
+          )
     );
   }
+}
+
+List<Pair<String, String>> search(List<Pair<String, String>> names, String source, int number_of_errors_per_word) {
+  List<Pair<String, String>> ret = [];
+
+  var l_source = source.split(" ");
+
+  for(var element in names){
+    String s = element.a;
+    var l_element = s.split(" ");
+
+    int i = 0, j = 0;
+    while (j < l_source.length && i < l_element.length) {
+      int m = minimumEditDistance(l_source[j], l_element[i]);
+      if (m < number_of_errors_per_word) {
+        j++;
+      }
+      i++;
+      if (j == l_source.length) {
+        ret.add(Pair<String, String>(s, element.b));
+      }
+    }
+  }
+
+  return ret;
+}
+
+
+int minimumEditDistance(String source, String target) {
+  int n = source.length;
+  int m = target.length;
+
+  // Create the dp matrix
+  List<List<int>> dp = List.generate(n + 1, (_) => List<int>.filled(m + 1, 0));
+
+  // Initialize the base cases
+  for (int i = 0; i <= n; i++) {
+    dp[i][0] = i;
+  }
+  for (int j = 0; j <= m; j++) {
+    dp[0][j] = j;
+  }
+
+  // Fill the dp matrix using dynamic programming
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      if (source[i - 1] == target[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = [dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]].reduce((a, b) => a < b ? a : b) + 1;
+      }
+    }
+  }
+
+  return dp[n][m];
 }
 
 /*
-class AllTeachersPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Lista de Docentes'),
-      ),
-      body: _buildListView(context),
-    );
-  }
+class ProfileDetails extends StatelessWidget {
+  final DocumentSnapshot document;
 
-  ListView _buildListView(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (_, index) {
-        return Card(
-          child: ListTile(
-            title: Text('The list item #$index'),
-            subtitle: Text('The subtitle'),
-            leading: Icon(Icons.thumb_up),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => TeacherPage(index.toString())));
-            },
-          ),
-        );
-      },
-    );
-  }
-}
-
-class TeacherPage extends StatelessWidget {
-  final String rowid;
-
-  TeacherPage(this.rowid);
-
-  final DocumentSnapshot document = newObject();
+  ProfileDetails(this.document);
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot document2) {
 
     return ListTile(
-      title: GestureDetector(
+        title: GestureDetector(
         onHorizontalDragEnd: (DragEndDetails details) {
           DocumentReference documentReference = FirebaseFirestore.instance.collection('comments').doc(document2.id);
           documentReference.delete();
@@ -142,7 +208,7 @@ class TeacherPage extends StatelessWidget {
             )
           ],
         ),
-      ),
+        ),
     );
   }
 
