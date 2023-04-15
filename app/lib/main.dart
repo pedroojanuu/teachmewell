2b5c1b15c7eef4e087db8edd4f1f6e6d9895c852
Joao Mota <motajoao22@gmail.com>
2023-04-15 15:18:17 +0100

    This commint will list all faculties in the main page

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -30,67 +30,138 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TeachMeWell',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'TeachMeWell'),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const Faculties(title: 'TeachMeWell'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+class Faculties extends StatelessWidget {
+  const Faculties({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  //Main page at the moment
   final String title;
 
-  // @override
-  // State<MyHomePage> createState() => _MyHomePageState();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          listTileTheme: const ListTileThemeData(
+            textColor: Colors.white,
+          )),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Faculdades')),
+        body: const LisTileExample(),
+      ),
+    );
+  }
+}
 
-  // ---------------------------------------
+
+class LisTileExample extends StatefulWidget {
+  const LisTileExample({super.key});
+
+  @override
+  State<LisTileExample> createState() => _LisTileExampleState();
+}
+
+class _LisTileExampleState extends State<LisTileExample>
+    with TickerProviderStateMixin {
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('faculdade').snapshots(),
+        builder: (context, snapshot) {
+          if(!snapshot.hasData) return const Text('Loading...');
+          return ListView.builder(
+            itemExtent: 80.0,
+            itemCount: (snapshot.data as QuerySnapshot).docs.length,
+            itemBuilder:  (context, index) =>
+                build_List_Item(context, (snapshot.data as QuerySnapshot).docs[index]),
+          );
+        }
+    );
+  }
+
+  Widget build_List_Item(BuildContext context, DocumentSnapshot document) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Hero(
+          tag: 'Faculdade',
+          // Wrap the ListTile in a Material widget so the ListTile has someplace
+          // to draw the animated colors during the hero transition.
+          child: Material(
+            child: ListTile(
+              title: Text(document["sigla"]),
+              subtitle: Text(document["nome"]),
+              tileColor: Colors.blue,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfessorsFeup(document, document["sigla"]),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class ProfessorsFeup extends StatelessWidget {
+  final DocumentSnapshot document;
+  final String faculdade;
+
+  ProfessorsFeup(this.document, this.faculdade);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Professores"),
+        ),
+        body: StreamBuilder(
+            stream: FirebaseFirestore.instance.collection('professor').where('faculdade', isEqualTo: faculdade).snapshots(),
+            builder: (context, snapshot) {
+              if(!snapshot.hasData) return const Text('Loading...');
+              return ListView.builder(
+                itemExtent: 80.0,
+                itemCount: (snapshot.data as QuerySnapshot).docs.length,
+                itemBuilder:  (context, index) =>
+                    _buildListItem(context, (snapshot.data as QuerySnapshot).docs[index]),
+              );
+            }
+        )
+    );
+  }
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
-    num acc = 0;
-    // make a query to find all comments in the comment collection, and add its rating to acc
-    FirebaseFirestore.instance.collection('teacher').doc(document.id).collection('comments').get().then((value) => {
-      value.docs.forEach((element) {
-        acc += element.get('rating');
-      })
-    });
 
     return ListTile(
       title: Row(
         children: [
           Expanded(
               child: Text(
-                document['name'],
-                style: Theme.of(context).textTheme.headlineMedium,
+                document['nome'],
+                style: Theme.of(context).textTheme.headlineSmall,
               ),
           ),
           Container(
             decoration: const BoxDecoration(
               color: Color(0xffddddff),
+              shape: BoxShape.circle,
             ),
-            padding: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(15.0),
             child: Text(
-              acc.toString(),
+              '0',
               style: Theme.of(context).textTheme.headlineMedium,
             )
           )
@@ -106,35 +177,135 @@ class MyHomePage extends StatelessWidget {
       }
     );
   }
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("TeachMeWell"),
-      ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('teacher').snapshots(),
-        builder: (context, snapshot) {
-          if(!snapshot.hasData) return const Text('Loading...');
-          return ListView.builder(
-            itemExtent: 80.0,
-            itemCount: (snapshot.data as QuerySnapshot).docs.length,
-            itemBuilder:  (context, index) =>
-                _buildListItem(context, (snapshot.data as QuerySnapshot).docs[index]),
-          );
-        }
-      )
-    );
-
-  }
-
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class ProfileDetails extends StatelessWidget {
   final DocumentSnapshot document;
 
   ProfileDetails(this.document);
+
+  @override
+  Widget build(BuildContext context) {
+    final description = TextEditingController();
+    final rating = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(document['nome']),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Wrap(
+              spacing: 10,
+              children: [
+                Container(
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.all(15.0),
+              child: CircleAvatar(
+                backgroundColor: Colors.orange,
+                radius: 55,
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage('https://sigarra.up.pt/${document['faculdade'].toString().toLowerCase()}/pt/FOTOGRAFIAS_SERVICE.foto?pct_cod=${document['codigo']}'),
+                  radius: 50,
+                  onBackgroundImageError: (e, s) {
+                    debugPrint('image issue, $e,$s');
+                  },
+                ),
+              ),
+            ),
+              Container(
+                margin: const EdgeInsets.all(10.0),
+                decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle
+                ),
+              ),
+              Container(
+                child: SizedBox(
+                  child: Text(document['nome'], style: Theme.of(context).textTheme.headlineSmall,),
+                ),
+              ),
+              SizedBox(
+                child: Text(document['faculdade'], style: Theme.of(context).textTheme.headlineSmall,),
+              ),
+              ]
+            ),
+
+          Container(
+              height: 220,
+              width: 400,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blue, width: 2),
+              ),
+              child: listComments(context),
+            ),
+            const Text('\n\nComment:'),
+            TextFormField(
+              controller: description,
+              decoration: const InputDecoration(
+                labelText: 'Description',
+              ),
+            ),
+            TextFormField(
+              controller: rating,
+              decoration: const InputDecoration(
+                labelText: 'Rating',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final d = description.text;
+                final rTemp = rating.text;
+                double r;
+
+                try {
+                  r = double.parse(rTemp);
+                  if(r >= 0 && r <= 5) {
+                    addComment(d, r);
+                    description.clear();
+                    rating.clear();
+                  }
+                  else {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('Erro de Input'),
+                        content: const Text('O valor do Rating deve ser um número entre 0 e 5!'),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'OK'),
+                            child: const Text('OK'),
+                          )
+                        ],
+                      ),
+                    );
+                  }
+                } catch (e) {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('Erro de Input'),
+                        content: const Text('O valor do Rating deve ser um número entre 0 e 5!'),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'OK'),
+                            child: const Text('OK'),
+                          )
+                        ],
+                      ),
+                  );
+                }
+              },
+              child : const Text('Submit'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot document2) {
 
@@ -145,102 +316,41 @@ class ProfileDetails extends StatelessWidget {
           documentReference.delete();
         },
         child:
-        Row(
-          children: [
-            Expanded(
-              child: Text(
-                document2['description'],
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ),
-            Container(
-                decoration: const BoxDecoration(
-                  color: Color(0xffddddff),
-                ),
-                padding: const EdgeInsets.all(10.0),
+          Row(
+            children: [
+              Expanded(
                 child: Text(
-                  document2['rating'].toString(),
+                  document2['description'],
                   style: Theme.of(context).textTheme.bodyMedium,
-                )
-            )
-          ],
-        ),
+                ),
+              ),
+              Container(
+                  decoration: const BoxDecoration(
+                    color: Color(0xffddddff),
+                  ),
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    document2['rating'].toString(),
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  )
+              )
+            ],
+          ),
         ),
     );
   }
 
   Future<dynamic> addComment(String description, double rating) async {
     final newDocument = FirebaseFirestore.instance.collection('comments').doc();
-    final json = { 'description': description, 'rating': rating, 'teacher': int.parse(document.id) };
+    final json = { 'description': description, 'rating': rating, 'teacher': int.parse(document['codigo'].toString()) };
 
     // Write to Firebase
     await newDocument.set(json);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final description = TextEditingController();
-    final rating = TextEditingController();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(document['name']),
-      ),
-      body: Container(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.network('https://sigarra.up.pt/feup/pt/FOTOGRAFIAS_SERVICE.foto?pct_cod=' + document.id),
-              Text(document['name'], style: Theme.of(context).textTheme.headlineMedium,),
-              Text(document['department'], style: Theme.of(context).textTheme.headlineSmall,),
-              Container(
-                height: 220,
-                width: 400,
-                child: listComments(context),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.blue, width: 2),
-                ),
-              ),
-              Text('\n\nComment:'),
-              TextFormField(
-                controller: description,
-                decoration: InputDecoration(
-                  labelText: 'Description',
-                ),
-              ),
-              TextFormField(
-                controller: rating,
-                decoration: InputDecoration(
-                  labelText: 'Rating',
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  final d = description.text;
-                  final r_temp = rating.text;
-                  double r;
-
-                  try {
-                    r = double.parse(r_temp);
-                    if(r >= 0 && r <= 5) {
-                      addComment(d, r);
-                      description.clear();
-                      rating.clear();
-                    }
-                  } catch (e) {}
-                },
-                child : Text('Submit'),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
   Widget listComments(BuildContext context) {
     return StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('comments').where('teacher', isEqualTo: int.parse(document.id) ).snapshots(),
+        stream: FirebaseFirestore.instance.collection('comments').where('teacher', isEqualTo: int.parse(document['codigo'].toString())).snapshots(),
         builder: (context, snapshot) {
           if(!snapshot.hasData) return const Text('Loading...');
           return ListView.builder(
@@ -253,70 +363,3 @@ class ProfileDetails extends StatelessWidget {
     );
   }
 }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-//
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
